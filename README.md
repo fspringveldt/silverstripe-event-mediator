e5d9e76473e51ec5ea9bb26f32019cddaaa4b0cb
Franco Springveldt <fspringveldt@gmail.com>
2017-01-08 00:36:15 +0200

    Completed afterCall aspect

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# silverstripe-event-mediator
-A Silverstripe event emitter based on the mediator pattern
+# SilverStripe Event Mediator
+A SilverStripe event emitter based on the mediator pattern, which can be applied to run a bit of code before or after any class method (i.e. triggered) using Silverstripe's [Aspects](https://docs.silverstripe.org/en/3.4/developer_guides/extending/aspects/)
+
+## Requirements
+```
+"silverstripe/framework": "3.*"
+"composer"
+```
+## Installation
+To install, run below from root of SilverStripe installation:
+```bash 
+> composer require fspringveldt/silverstripe-event-mediator
+``` 
+
+http://**your-site-url**?flush=1 once composer is complete the flush the manifest.
+
+##Configuration
+Let's say after each call to ```$A->foo()``` you'd like to fire off a call to ```$B->bar()```, the following in your composer.yml should do the trick:
+```yaml
+Injector:
+  EventMediator:
+    properties:
+      events:
+        foo:
+          triggerBar:
+            class: B
+            method: bar
+  ProxiedA:
+    class: A
+  A:
+    class: AopProxyService
+    properties:
+      proxied: %$ProxiedA
+      afterCall:
+        foo:
+          - %$EventMediator
+```
+All parameters from ```$A->foo()``` are sent through ```$B->foo()``` for your perusal.
---
+++ code/EventMediator.php
@@ -0,0 +1,68 @@
+<?php
+
+
+	/**
+	 * Created by PhpStorm.
+	 * User: francospringveldt
+	 * Date: 2016/12/31
+	 * Time: 10:01 PM
+	 */
+
+	/**
+	 * Class EventMediator
+	 * @package
+	 *
+	 * This class employs the Mediator design pattern, which is called after methods defined in
+	 * the yaml config
+	 */
+	class EventMediator implements AfterCallAspect
+	{
+		public $events = [];
+
+		public function attach($eventName, $callback)
+		{
+			if(!isset($this->events[$eventName]))
+			{
+				$this->events[$eventName] = [];
+			}
+			$this->events[$eventName][] = $callback;
+		}
+
+		/**
+		 * @param      $eventName
+		 * @param null $data
+		 */
+		public function trigger($eventName, $data = NULL)
+		{
+			foreach($this->events[$eventName] as $name => $obj)
+			{
+
+				$observer = Injector::inst()
+									->create($obj['class']);
+				call_user_func_array(
+					[
+						$observer,
+						$obj['method'],
+					], $data
+				);
+
+			}
+		}
+
+		/**
+		 * Call this aspect after a method is executed
+		 *
+		 * @param object $proxied
+		 *                The object having the method called upon it.
+		 * @param string $method
+		 *                The name of the method being called
+		 * @param string $args
+		 *                The arguments that were passed to the method call
+		 * @param mixed  $result
+		 *                The result of calling the method on the real object
+		 */
+		public function afterCall($proxied, $method, $args, $result)
+		{
+			$this->trigger($method, $args[0]);
+		}
+	}
